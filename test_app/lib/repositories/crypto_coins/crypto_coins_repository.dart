f0cab71d53c2ff712ffec42f623ca6363b77d831
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-19 17:25:34 +0400

    added

--- test_app/lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ test_app/lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:test_app/repositories/crypto_coins/models/crypto_coin.dart';
 
 class CryptoCoinsRepository{
-  Future<void> getCoinsList() async{ //асинхронный метод который не известно когда закончится
+  Future<List<CryptoCoin>> getCoinsList() async{ //асинхронный метод который не известно когда закончится
     final response = await Dio().get (
       'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BNB&tsyms=USD'
     );
-    debugPrint(response.toString());
+    debugPrint(response.toString()); 
   }
 }
