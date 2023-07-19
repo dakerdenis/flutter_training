49ef3e3fae88cc636648da908a90c5826cab0d3c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-19 09:24:57 +0400

    added

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -14,25 +13,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
-        listTileTheme: const ListTileThemeData(iconColor: Color.fromARGB(255, 214, 166, 20)),
-        appBarTheme: const AppBarTheme(backgroundColor:  Color.fromARGB(255, 214, 166, 20),
-        titleTextStyle:TextStyle(color: Colors.black, fontSize: 23) ),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontWeight: FontWeight.w500,
-            fontSize: 20
-          ),
-          bodySmall: TextStyle(
-            color: Color.fromARGB(192, 255, 255, 255),
-            fontWeight: FontWeight.w300,
-            fontSize: 14
-          )
-        )
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+          listTileTheme: const ListTileThemeData(
+              iconColor: Color.fromARGB(255, 214, 166, 20)),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Color.fromARGB(255, 214, 166, 20),
+              titleTextStyle: TextStyle(color: Colors.black, fontSize: 23)),
+          textTheme: const TextTheme(
+              bodyMedium: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20),
+              bodySmall: TextStyle(
+                  color: Color.fromARGB(192, 255, 255, 255),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 14))),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -48,13 +45,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,30 +56,29 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: ListView.separated(
-        itemCount: _counter,
-        separatorBuilder: (context, index)=> const Divider(),
-        itemBuilder: (context,i)=>ListTile(
-          leading:  Image.asset('assets/images/bitcoin.png', height: 25,width: 25,),
-          title:  Text(
+        itemCount: 10,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, i) => ListTile(
+          leading: Image.asset(
+            'assets/images/bitcoin.png',
+            height: 25,
+            width: 25,
+          ),
+          title: Text(
             "Bitcoins",
-            style: theme.textTheme.bodyMedium,),
-            subtitle: Text(
-              "2000\$",
-              style: theme.textTheme.bodySmall,
-            ),
-            trailing: Icon(Icons.arrow_forward_ios),
+            style: theme.textTheme.bodyMedium,
+          ),
+          subtitle: Text(
+            "2000\$",
+            style: theme.textTheme.bodySmall,
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
         child: Container(height: 50.0),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
