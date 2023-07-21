ebe6c6a2c7ac3b285b0832077b5c4f9ca1697cdd
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-21 10:17:45 +0400

    added

--- test_app/lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ test_app/lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -2,11 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:test_app/repositories/crypto_coins/models/crypto_coin.dart';
 
-class CryptoCoinsRepository{
-  Future<List<CryptoCoin>> getCoinsList() async{ //асинхронный метод который не известно когда закончится
-    final response = await Dio().get (
-      'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BNB&tsyms=USD'
-    );
-    debugPrint(response.toString()); 
+class CryptoCoinsRepository {
+  Future<List<CryptoCoin>> getCoinsList() async {
+    //асинхронный метод который не известно когда закончится
+    final response = await Dio().get(
+        'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BNB&tsyms=USD');
+    final data = response.data as Map<String, dynamic>;
+
+    final dataList = data.entries.map((e) => CryptoCoin(
+          name: e.key,
+          priceInUSD: (e.value as Map<String, dynamic>)['USD'],
+        )).toList(); //entries  -вернёт всё в виде словаря, метод мар прохоидт по всему списку и что то делает с данными
+      return dataList;
   }
 }
