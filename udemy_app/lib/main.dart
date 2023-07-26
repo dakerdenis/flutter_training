de39d57c7837a8d3810a45b6ca63a5aad3e417d6
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-26 21:23:10 +0300

    added

--- udemy_app/lib/main.dart
+++ udemy_app/lib/main.dart
@@ -2,10 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World !'),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.amber,
+                Colors.white30,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World !',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.5,
+                wordSpacing: 2.0,
+              ),
+            ),
+          ),
         ),
       ),
     ),
--- udemy_app/test/widget_test.dart
+++ udemy_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:udemy_app/myApp.dart';
+import 'package:udemy_app/main_container/container.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
