2553997d99f2361873ee57c668ed96e5a06164b2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-27 12:39:48 +0400

    added

--- udemy_app/lib/main_container/gradient_container.dart
+++ udemy_app/lib/main_container/gradient_container.dart
@@ -4,34 +4,50 @@ import 'package:udemy_app/main_container/styled_text.dart';
 const startAlignmen = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(
-    this.color1,
-    this.color2,
-     {
-    super.key
-  });
+  const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
+
+  void rollDice(){
+
+  }
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors:  [color1, color2],
+          colors: [color1, color2],
           begin: startAlignmen,
           end: endAlignment,
         ),
       ),
-      child:  Center(
-       // child: StyledText('Stalker'),
-        child: Image.asset('assets/images/dice-1.png'),
-      ),
+      child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/dice-1.png',
+            width: 200,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+              textStyle: const TextStyle(
+                fontSize: 28
+              )
+            ),
+            child: const Text('Roll Dice'),
+
+          ),
+        ],
+      )
+          // child: StyledText('Stalker'),
+
+          ),
     );
   }
 }
-
-
