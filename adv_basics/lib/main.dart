c752106dc13df9e18ad95d7d0a8b978e2325dea6
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-28 14:40:40 +0400

    added

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 74, 4, 114),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('assets/images/quiz-logo.png', width: 200, height: 200),
+              Image.asset('assets/images/quiz-logo.png',
+                  width: 200, height: 200),
               SizedBox(height: 20),
               const Text(
                 'Hello, Flutter!',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-              
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, width: 1
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: ElevatedButton(
+                    on
+                  ),
+                )
             ],
           ),
         ),
