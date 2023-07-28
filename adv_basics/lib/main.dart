365652ef06490b56705c5dac7c1e9b84ea7cda7c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-28 11:53:44 +0400

    added

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 74, 20, 161),
-      body: Column(
-        // ignore: unnecessary_const
-        children: [const Text('data'), Image.asset('assets/images/quiz-logo.png')],
-      )
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/quiz-logo.png', width: 200, height: 200),
+              SizedBox(height: 20),
+              const Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
