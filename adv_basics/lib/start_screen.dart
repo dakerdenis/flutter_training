9e2784fecba9854544d8ef66e900e4a466902f89
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-07-31 13:15:36 +0400

    added

---
+++ adv_basics/lib/start_screen.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+              width: 200, height: 200),
+          SizedBox(height: 40),
+          const Text(
+            'Hello, Flutter!',
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(),
+            child: ElevatedButton(
+              onPressed: () {
+                // ignore: avoid_print
+                print('Button Pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(0, 255, 255, 255),
+                onPrimary: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      8), // Border radius for the button
+                ),
+              ),
+              child: const Text(
+                'Start Quiz',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
