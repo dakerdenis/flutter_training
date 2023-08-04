e96866e118a3f867b28931d3d30304c1e8435acc
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-05 00:24:32 +0400

    added

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,6 +1,8 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,8 +15,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
-
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -25,6 +26,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if(selectedAnswers.length == questions.length){
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'results-screen';
+      });
+    }
   }
 
   @override
@@ -35,6 +43,10 @@ class _QuizState extends State<Quiz> {
       screenWidget = QuestionsScreen(
         onSelectedAnswer: chooseAnswer,
       );
+    }
+
+    if(activeScreen == 'results-screen'){
+      screenWidget = ResultsScreen();
     }
 
     return MaterialApp(
