4231b2003e72cdbf5da3c44324b72ddd0c942aea
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-07 19:33:18 +0300

    added

--- adv_basics/lib/questions_summary.dart
+++ adv_basics/lib/questions_summary.dart
@@ -12,42 +12,60 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map(
             (data) {
-              return Row(
+              //? ROW EBANIY
+              return Column(
                 children: [
-                  //! EBANIY TEXT V CIRCLE
-                  Container(
-                    height: 30,
-                    width: 30,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          ((data['question_index'] as int) + 1).toString(),
+                  Row(
+                    children: [
+                      //! EBANIY TEXT V CIRCLE
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue,
                         ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              ((data['question_index'] as int) + 1).toString(),
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      //! SPACE BETWEEN 
+                      const SizedBox(
+                        width: 30,
+                      ),
 
-                  //! QUESTIONS AND ANSWERS
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(data['question'] as String),
-                        const SizedBox(
-                          height: 5,
+                      //! QUESTIONS AND ANSWERS
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left
+                            children: [
+                              //! QUESTION
+                              Text(data['question'] as String),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              //!USER ANSWER
+                              Text(data['user_answer'] as String),
+                              //!CORRECT ANSWER
+                              Text(data['correct_answer'] as String),
+                            ],
+                          ),
                         ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
+                  const SizedBox(height: 20,)
                 ],
               );
             },
