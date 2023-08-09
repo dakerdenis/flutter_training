34cee9dbadc3d3c4bee0ecbfbb570a786568e78e
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-09 13:11:54 +0300

    added

--- adv_basics/lib/questions_summary/questions_summary.dart
+++ adv_basics/lib/questions_summary/questions_summary.dart
@@ -1,4 +1,5 @@
-import 'package:adv_basics/questions_summary/question_identifier.dart';
+
+import 'package:adv_basics/questions_summary/summary_item.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
@@ -7,65 +8,16 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(BuildContext context) {
-
-
-
-
-
     return SizedBox(
       height: 300,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map(
             (data) {
-
-
-
               //? ROW EBANIY
-              return Column(
+              return  Column(
                 children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start, // Align children to the top
-                    children: [
-                      //! EBANIY TEXT V CIRCLE
-                      const QuestionIdentifier(isCOrrectAnswer: null, questionIndex: null,),
-                      //! SPACE BETWEEN
-                      const SizedBox(
-                        width: 30,
-                      ),
-
-                      //! QUESTIONS AND ANSWERS
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment
-                              .start, // Align children to the left
-                          children: [
-                            //! QUESTION
-                            Text(
-                              data['question'] as String,
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 240, 209, 255),
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            //!USER ANSWER
-                            Text(data['user_answer'] as String,
-                            style: const TextStyle(
-                              color: Color.fromARGB(174, 230, 180, 255),
-                            ),),
-                            //!CORRECT ANSWER
-                            Text(data['correct_answer'] as String,
-                            style: const TextStyle(
-                              color: Color.fromARGB(181, 165, 255, 225),
-                            ),),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                  SummaryItem(data),
                   const SizedBox(
                     height: 20,
                   )
@@ -78,4 +30,5 @@ class QuestionsSummary extends StatelessWidget {
     );
   }
 }
+
 
---
+++ adv_basics/lib/questions_summary/summary_item.dart
@@ -0,0 +1,62 @@
+
+import 'package:adv_basics/questions_summary/question_identifier.dart';
+import 'package:flutter/material.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(
+    this.itemData,
+    {
+    super.key,
+  });
+
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer = itemData['user_answer'] == itemData['correct_answer'];
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start, // Align children to the top
+      children: [
+        //! EBANIY TEXT V CIRCLE
+         QuestionIdentifier(isCOrrectAnswer: isCorrectAnswer,
+         questionIndex: itemData['question_index'] as int,),
+        //! SPACE BETWEEN
+        const SizedBox(
+          width: 30,
+        ),
+
+        //! QUESTIONS AND ANSWERS
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment
+                .start, // Align children to the left
+            children: [
+              //! QUESTION
+              Text(
+                itemData['question'] as String,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 240, 209, 255),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              //!USER ANSWER
+              Text(itemData['question'] as String,
+              style: const TextStyle(
+                color: Color.fromARGB(174, 230, 180, 255),
+              ),),
+              //!CORRECT ANSWER
+              Text(itemData['correct_answer'] as String,
+              style: const TextStyle(
+                color: Color.fromARGB(181, 165, 255, 225),
+              ),),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
