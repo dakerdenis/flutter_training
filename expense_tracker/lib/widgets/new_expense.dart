ac5fee1b5f1b80f14d56142715e9bc61dc83bcba
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-18 13:39:53 +0400

    added

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -10,10 +10,31 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
+  // var _enteredTitle = '';
+//
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
 
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+//dispose like initstate and build is part of a statefulwidget's lifecycle
+// It's called auto by flutter when the widget it's state are about to be destroyed
+//removed from the UI
+
+  //only "state" classes can implement this dispose method (StatelessWidget can't)
+  // That's why you should use a StatefulWidget here
+  //should dispose all textediting controller
+  final _titleController = TextEditingController();
+  final _priceCOntroller = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _priceCOntroller.dispose();
+    super.dispose();
+  }
+
+  void resetData(){
+
   }
 
   @override
@@ -23,21 +44,42 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.text,
             decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _priceCOntroller,
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text('Price'),
+            ),
+          ),
           Row(
             children: [
+              const SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: () {
                   // ignore: avoid_print
-                  print(_enteredTitle);
+                  print(_titleController.text);
+                  // ignore: avoid_print
+                  print(_priceCOntroller.text);
                 },
-                child: Text('Save Expense'),
+                child: const Text('Save Expense'),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  _titleController.clear();
+                  _priceCOntroller.clear();
+                },
+                child: const Text('reset'),
               )
             ],
           )
