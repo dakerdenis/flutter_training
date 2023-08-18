8a90e65392f06d8dd5cdba598f8e1fb1aea7a49a
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-18 09:56:42 +0400

    added

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,8 +1,22 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum Category {
+  food,travel,leisure,work
+}
+//kind of string type values
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date});
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final String category;
 }
