a97e00928038938684ae4ab3ff71cfd9c098b2e0
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-22 13:15:01 +0400

    added

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -65,6 +65,7 @@ class _NewExpenseState extends State<NewExpense> {
         amount: enteredAmount,
         date: _selectedDate!, //! ! - this mark telling dart that we're sure that it'll not be null
         category: _selectedCategory));
+        Navigator.pop(context);
   }
 
   @override
@@ -93,7 +94,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16,48,16,16),
       child: Column(
         children: [
           TextField(
