043bbe9d9c3467ca12fa600e4f3f063778f08250
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-06 23:04:45 +0400

    added

--- pets_universe/lib/main/home.dart
+++ pets_universe/lib/main/home.dart
@@ -15,7 +15,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.amber,
+
         padding: const EdgeInsets.only(top: 40, bottom: 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -46,7 +46,6 @@ class _HomeState extends State<Home> {
             ),
             Expanded(
               child: Container(
-                color: Colors.blue,
                 child: Stack(
                   children: [
                     Positioned(
@@ -73,11 +72,11 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                                        Positioned(
-                      bottom: MediaQuery.of(context).size.height * 0.2,
+                    Positioned(
+                      bottom: MediaQuery.of(context).size.height * 0.4,
                       right: 20.0,
                       child: Transform.rotate(
-                        angle: -90 * 3.1415927 / 180,
+                        angle: 90 * 3.1415927 / 180,
                         child: Image.asset(
                           'assets/pu_landing2.png',
                           width: 100,
@@ -85,17 +84,29 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                                                            Positioned(
-                      bottom: MediaQuery.of(context).size.height * 0.2,
-                      right: 20.0,
+                    Positioned(
+                      bottom: 0,
+                      right: 30.0,
                       child: Transform.rotate(
-                        angle: -90 * 3.1415927 / 180,
-                        child: Image.asset(
-                          'assets/pu_landing2.png',
-                          width: 100,
-                          height: 100,
-                        ),
-                      ),
+                          angle: 0 * 3.1415927 / 180,
+                          child: Transform.scale(
+                            scale: 1.1,
+                            child: Image.asset(
+                              'assets/cat1.png',
+                            ),
+                          )),
+                    ),
+                    Positioned(
+                      bottom: MediaQuery.of(context).size.height * 0.27,
+                      right:  MediaQuery.of(context).size.height * 0.065,
+                      child: Transform.rotate(
+                          angle: 0 * 3.1415927 / 180,
+                          child: Transform.scale(
+                            scale: 1,
+                            child: Image.asset(
+                              'assets/cloud.png',
+                            ),
+                          )),
                     ),
                   ],
                 ),
