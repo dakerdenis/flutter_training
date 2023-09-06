21bdadbc8507bd1608e99b0f4ffdd5317cc9cd27
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-06 11:54:16 +0400

    added

--- pets_universe/lib/main/login.dart
+++ pets_universe/lib/main/login.dart
@@ -18,117 +18,164 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        //!-------TOP BUTTON WHICH CLOSES LOGIN WIDGET
-        Row(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('stalker')),
-          ],
-        ),
-        //!------CENTER IMAGE OF LOGO AND DOG
-        SizedBox(
-            width: 200,
-            height: 150,
-            child: Center(
-              child: Image.asset(
-                'assets/323.png',
-                width: 128,
-                height: 134,
-                fit: BoxFit.cover,
-              ),
-            )),
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //!-------TOP BUTTON WHICH CLOSES LOGIN WIDGET
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('stalker')),
+            ],
+          ),
+          //!------CENTER IMAGE OF LOGO AND DOG
+          SizedBox(
+              width: 200,
+              height: 150,
+              child: Center(
+                child: Image.asset(
+                  'assets/323.png',
+                  width: 128,
+                  height: 134,
+                  fit: BoxFit.cover,
+                ),
+              )),
 
-        ///!-------EMAIL ADRESSS
-        Container(
-          width: double.infinity,
-          height: 52,
-          child: FractionallySizedBox(
-            widthFactor: 0.8,
-            heightFactor: 1,
-            child: Container(
-
-              decoration: BoxDecoration(
-
-                borderRadius: BorderRadius.circular(5.0),
-                gradient: const LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color.fromRGBO(142, 36, 170, 1),
-                      Color.fromRGBO(255, 235, 59, 1),
-                    ]),
-              ),
-              child:  FractionallySizedBox(
-                widthFactor: 0.98,
-                heightFactor: 0.98,
-                child:  Container(
-                  color: Colors.white,
-
+          ///!-------EMAIL ADRESSS
+          SizedBox(
+            width: double.infinity,
+            height: 52,
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              heightFactor: 1,
+              child: Container(
+                padding: const EdgeInsets.all(1.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  gradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color.fromRGBO(255, 235, 59, 1),
+                        Color.fromRGBO(142, 36, 170, 1),
+                      ]),
+                ),
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  heightFactor: 1,
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                    ),
+                    child: const Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Enter your email:',
+                              style: TextStyle(
+                                color: Color.fromRGBO(95, 95, 95, 1),
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        //!----------BOTTOM CONTAINER WITH REGISTRATION LINK
-        Container(
-          width: double.infinity,
-          height: 60.0,
-          decoration: const BoxDecoration(
-              color: Color.fromRGBO(206, 145, 205, 0.1),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              )),
-          child: Center(
-            child: RichText(
-              text: TextSpan(
-                  text: 'Don’t have an account? ',
-                  style: const TextStyle(
-                    color: Color.fromRGBO(51, 51, 51, 1),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: ' Create one!',
-                      style: const TextStyle(
-                        color: Color.fromRGBO(206, 145, 205, 1),
+          ////!---------Continue button------------
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              heightFactor: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(255, 247, 0, 1),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child:  const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Continue',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
                       ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = _handleLinkTap,
-                    )
-                  ]),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        )
+          //!----------BOTTOM CONTAINER WITH REGISTRATION LINK
+          Container(
+            width: double.infinity,
+            height: 60.0,
+            decoration: const BoxDecoration(
+                color: Color.fromRGBO(206, 145, 205, 0.1),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                )),
+            child: Center(
+              child: RichText(
+                text: TextSpan(
+                    text: 'Don’t have an account? ',
+                    style: const TextStyle(
+                      color: Color.fromRGBO(51, 51, 51, 1),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: ' Create one!',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(206, 145, 205, 1),
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = _handleLinkTap,
+                      )
+                    ]),
+              ),
+            ),
+          )
 
-        //  //! картинка
-        //  AspectRatio(
-        //    aspectRatio: 1.0,
-        //    child: Center(
-        //      child: Image.asset(
-        //        height: 100,
-        //        'assets/123.jpg',
-        //        fit: BoxFit.contain,
-        //      ),
-        //    ),
-        //  ),
-        //  //! ввод данных
-        //  TextField(
-        //    controller: _logincontroller,
-        //    maxLength: 40,
-        //    keyboardType: TextInputType.text,
-        //    decoration: const InputDecoration(
-        //      label: Text('Title'),
-        //    ),
-        //  )
-      ],
+          //  //! картинка
+          //  AspectRatio(
+          //    aspectRatio: 1.0,
+          //    child: Center(
+          //      child: Image.asset(
+          //        height: 100,
+          //        'assets/123.jpg',
+          //        fit: BoxFit.contain,
+          //      ),
+          //    ),
+          //  ),
+          //  //! ввод данных
+          //  TextField(
+          //    controller: _logincontroller,
+          //    maxLength: 40,
+          //    keyboardType: TextInputType.text,
+          //    decoration: const InputDecoration(
+          //      label: Text('Title'),
+          //    ),
+          //  )
+        ],
+      ),
     );
   }
 }
