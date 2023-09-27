5559abd316b4a1e283df125d82ca9244ff97ad4e
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-27 15:04:29 +0400

    added

---
+++ meals/lib/widgets/categories_grid_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryFridItem extends StatelessWidget{
+  const CategoryFridItem({super.key required this.category});
+
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
