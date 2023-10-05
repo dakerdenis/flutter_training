d66a1ac6f5313f261a025f0336f95d844f0e7df8
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-05 11:59:48 +0400

    added

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(
+          title: 'Some title',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +31,13 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisSpacing: 20,
             mainAxisSpacing: 20,
           ),
-          children:  [
+          children: [
             //availableCategories.map ((category))
-            for(final category in availableCategories)
-              CategoryGridItem(category: category)
+            for (final category in availableCategories)
+              CategoryGridItem(category: category, onSelectCategory: (){
+                _selectCategory(context);
+              },)
           ],
         ));
   }
 }
-
-
-
-
